133cdc7384cdddfc93b4eb1f59cc274a0149bc6a
Alire Jafari <asjn3e@gmail.com>
2023-04-22 18:31:51 +0400

    fixed morali server start bug

--- Back-End/index.js
+++ Back-End/index.js
@@ -7,16 +7,15 @@ const { EvmChain } = require("@moralisweb3/common-evm-utils");
 const app = express();
 const port = 5000;
 
+Moralis.start({
+  apiKey: "f3pTmwFHKJJ00wZfvNSQfQGwQM9c6kFA1CZiISy3IQBRC5HbfMNvEpA1ADwYM0oM",
+});
 // Add a variable for the api key, address and chain
-const MORALIS_API_KEY =
-  "f3pTmwFHKJJ00wZfvNSQfQGwQM9c6kFA1CZiISy3IQBRC5HbfMNvEpA1ADwYM0oM";
-const address = "0x5199d6204B788016353A201b8DAd4668a71F1A8a";
-const chain = EvmChain.ETHEREUM;
 
 app.get("/wallet/:walletAddress/:chain", async (req, res) => {
   try {
     // Get and return the crypto data
-    const data = await getData();
+    const data = await getData(req.params.walletAddress, req.params.chain);
 
     res.status(200);
     res.json(data);
@@ -39,10 +38,15 @@ const startServer = async () => {
 startServer();
 
 //Getting the data (balance and price of the coin)
-async function getData() {
-  await Moralis.start({
-    apiKey: MORALIS_API_KEY,
-  });
+async function getData(address, requestedChain) {
+  let chain = EvmChain.ETHEREUM;
+  if (requestedChain == "eth") {
+    chain = EvmChain.ETHEREUM;
+  } else if (requestedChain == "bsc") {
+    chain = EvmChain.BSC;
+  } else {
+    chain = EvmChain.POLYGON;
+  }
 
   // Get token balances
   const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
